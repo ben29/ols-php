34234b27d1c7ed418b0a83265e6364b058c69b05
Ben Hakim <ben@hakim.co.il>
2024-10-27 11:49:56 +0200

    try

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,8 +5,12 @@ fi
 if [ -z "$(ls -A -- "/usr/local/lsws/admin/conf/")" ]; then
 	cp -R /usr/local/lsws/admin/.conf/* /usr/local/lsws/admin/conf/
 fi
-chown 994:994 /usr/local/lsws/conf -R
-chown 994:1001 /usr/local/lsws/admin/conf -R
+#chown 994:994 /usr/local/lsws/conf -R
+#chown 994:1001 /usr/local/lsws/admin/conf -R
+chown -R lsadm:lsadm /usr/local/lsws/admin/conf/htpasswd
+chmod 644 /usr/local/lsws/admin/conf/htpasswd
+chown -R lsadm:lsadm /usr/local/lsws/admin/conf/
+chmod 755 /usr/local/lsws/admin/conf/
 
 /usr/local/lsws/bin/lswsctrl start
 $@
